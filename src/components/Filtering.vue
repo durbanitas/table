f2f264881bf6d671fa64abee9b974edf052211e1
durbanitas <durbanitas@protonmail.com>
2022-04-21 15:03:07 +0200

    test fix filtering

--- src/components/Filtering.vue
+++ src/components/Filtering.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { watch } from 'vue';
+import { watch } from 'vue'
 
 const props = defineProps({
   headers: {
@@ -9,8 +9,7 @@ const props = defineProps({
 const OPERATORS = ['==', '>', '<']
 const TIMEOUT = 400
 
-// TODO: add autofocus
-// TODO: add input validation
+// TODO: add input types validations
 
 const emit = defineEmits(['submit', 'remove'])
 const filtersScope = $ref([])
@@ -24,35 +23,32 @@ function addFilter () {
     value: ''
   }
   filtersScope.push(filterObj)
-  isValid.push(true)
+  inputValids.push(true)
 }
-function removeFilter (idx) {
-  itemRefs.splice(idx, 1);
-  isValid.splice(idx, 1)
-  filtersScope.splice(idx, 1)
-  filterTags = filtersScope.filter(f => f.value.length)
-  emit('submit', filterTags)
-}
-
+// set autofocus
 watch(() => [...itemRefs], (oldVal, newVal) => setFocus(oldVal, newVal))
 function setFocus (oldVal, newVal) {
   const oldArrLen = oldVal.filter(el => el !== null).length
   const newArrLen = newVal.filter(el => el !== null).length
   if (newArrLen < oldArrLen) itemRefs[newArrLen].focus()
 }
+function removeFilter (idx) {
+  itemRefs.splice(idx, 1);
+  inputValids.splice(idx, 1)
+  filtersScope.splice(idx, 1)
+  filterTags = filtersScope.filter(f => f.value.length)
+  emit('submit', filterTags)
+}
 
-function emitValue () {
-  const allValid = isValid.findIndex(valid => valid === false) < 0
-  if (allValid) {
-    itemRefs.forEach((input, idx) => {
-      if (input !== null) filtersScope[idx].value = input.value
-    })
+function emitValue (inputVal, idx) {
+  let isValidInput = validate(inputVal) || inputVal.length === 0
+  if (isValidInput) {
+    filtersScope[idx].value = inputVal
     filterTags = filtersScope.filter(f => f.value.length)
     emit('submit', filterTags)
+  } else {
+    inputValids[idx] = false
   }
-  // else {
-  //   console.error('wrong input value(s)')
-  // }
 }
 const debounce = (func, wait) => {
   let timeout
@@ -65,17 +61,13 @@ const debounce = (func, wait) => {
     timeout = setTimeout(later, wait)
   }
 }
-const updateValue = debounce(() => emitValue(), TIMEOUT)
+const updateValue = debounce((e, idx) => emitValue(e.target.value, idx), TIMEOUT)
 
 // validations
-function validateInput (e, idx) {
-  let isNumber = true
-  if (e.target.value.length) {
-    isNumber = !isNaN(parseFloat(e.target.value)) && isFinite(e.target.value)
-  }
-  isValid[idx] = isNumber
+const inputValids = $ref([])
+function validate (val) {
+  return !isNaN(parseFloat(val)) && isFinite(val)
 }
-const isValid = $ref([])
 </script>
 
 <template>
@@ -90,12 +82,11 @@ const isValid = $ref([])
         <option v-for="operator in OPERATORS" :key="operator.id" v-text="operator" />
       </select>
       <!-- filter by value -->
-      <input type="text" @keyup="updateValue" :ref="(input) => { itemRefs[idx] = input }"
-        @change="validateInput($event, idx)" @input="validateInput($event, idx)" :value="filter.value">
+      <input type="text" @keyup="updateValue($event, idx)" :ref="(input) => { itemRefs[idx] = input }">
       <!-- remove filter -->
       <button @click="removeFilter(idx)" v-text="'x'" />
       <!-- show invalid message -->
-      <div class="error" v-if="!isValid[idx]">invalid value</div>
+      <div class="error" v-if="!inputValids[idx]">invalid value</div>
     </div>
 
     <button @click="addFilter()" v-text="'add filter +'" />
